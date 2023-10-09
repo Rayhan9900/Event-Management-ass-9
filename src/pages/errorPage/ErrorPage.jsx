51fd996edfd1c0e71fb4e2c3f630c4b4400a64b2
rayhan <rayhanmahmud990099@gmail.com>
2023-10-09 16:29:03 +0600

    added the error page done

--- src/pages/errorPage/ErrorPage.jsx
+++ src/pages/errorPage/ErrorPage.jsx
@@ -1,13 +1,13 @@
 
 import { Link } from 'react-router-dom'
-import error from '../../assets/Error.jpg'
+import error from '../../assets/404_page_cover.jpg'
 
 function ErrorPage() {
     return (
-        <div>
-            <img className='w-full absolute' src={error} alt="" />
+        <div >
+            <img className='w-full h-full  fixed' src={error} alt="" />
 
-            <Link className='relative bottom-1/2 left-1/3' to="/" ><button>Go To Home Page</button></Link>
+            <Link className='  mt-10 fixed' to="/" ><button className='text-accent'>Go To Home Page</button></Link>
         </div>
     )
 }
